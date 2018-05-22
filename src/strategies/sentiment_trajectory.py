e71d30a26d277bcd061ea7e1c574fea132981fdd
Michiel <michiel.baptist@student.kuleuven.be>
2018-05-22 23:45:52 +0200

    Fixed the thing also changed the evaluator a bit looks cooler now also don't forget to install nltk and download the vader sentiment thing

--- src/strategies/sentiment_trajectory.py
+++ src/strategies/sentiment_trajectory.py
@@ -41,31 +41,23 @@ class SentimentTracjectoryStrategy(Strategy):
         # nothing makes sense! Except for the last sentence which by definition
         partial_stories = self.group_stories(partial_stories, self.story_grouping[:-1])
         
+        sentiment_for_endings = self.find_trajectories(endings)
+        ending_distributions = self.find_ending_distribution(partial_stories)
         
-        actual_endings_for_options = self.find_trajectories(endings)
-        most_likely_endings = self.find_most_likely_endings(partial_stories)
+        #TODO: fix the code a bit
+        endings_probabilities = []
+        for i, endings in enumerate(sentiment_for_endings):
+            distribution = ending_distributions[i]
+            mapped_ending = [distribution[ending] for ending in endings]
+            endings_probabilities.append(mapped_ending)        
+        endings_probabilities = np.array(endings_probabilities)
         
-        distance = np.abs((actual_endings_for_options.T - most_likely_endings).T)
-        
-        same = 0
-        for d in distance:
-            if d[0] == d[1]:
-                same += 1
-        print(same, len(data))
-        
-        closest = np.argmin(distance, axis=1) + 1
-        
-        return closest
+        # +1 to select sentence
+        return np.argmax(endings_probabilities, axis=1) + 1 
 
-    def find_most_likely_endings(self, partial_stories):
+    def find_ending_distribution(self, partial_stories):
         partial_trajectories = self.find_trajectories(partial_stories)
-        #partial_trajectories = np.sign(partial_trajectories).astype(int) + 1
-        
-        print(partial_trajectories)
-        max = np.apply_along_axis(lambda x: np.argmax(self.counts[tuple(x)]),
-                                                        1, partial_trajectories)
-        print(max)
-        return max
+        return [self.counts[tuple(partial_trajectory)] for partial_trajectory in partial_trajectories]
         
     def count_elements(self, objs, n_values, smoothing=1):
         #Method assumes that values are in range [0,n_values] (n_values excluded)
@@ -99,7 +91,7 @@ class SentimentTracjectoryStrategy(Strategy):
         self.done +=1
         if self.done % 1000 == 0:
             print("Done {0}".format(self.done))
-            print(trajectory)
+        self.done=0
         #Sanity
         
         return trajectory
