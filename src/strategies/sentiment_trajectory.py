95e7bc4302e9e2ea40f759d0a3343a1c85cc1dc0
MichielBaptist <michiel.baptist@gmail.com>
2018-05-23 14:44:04 +0200

    Changed classifier to random forrest instead of counting, big surprise nothing much changed which is normal i guess

--- src/strategies/sentiment_trajectory.py
+++ src/strategies/sentiment_trajectory.py
@@ -1,6 +1,7 @@
 import numpy as np
 import random
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
+from sklearn.ensemble import RandomForestClassifier as rf
 
 from strategies import Strategy
 
@@ -11,59 +12,69 @@ class SentimentTracjectoryStrategy(Strategy):
         self.story_grouping = (1,2,1,1) #Assume beginning -> body -> climax -> ending
         self.n_values = 3   #Negative, Neutral and Positive
         self.done = 0 #sanity
+        self.n_sentences = np.sum(self.story_grouping) #How many sentences to consider?
         
         #Decompose the data 
         IDs = data[:,0]
         titles = data[:,1]
-        stories = data[:,2:]
+        stories = data[:,-self.n_sentences:]        
         
-        #Group sentences
+        self.classifier = rf()
+        
+        #Group sentences to form blocks for sentiment
         stories = self.group_stories(stories, self.story_grouping)
         
         #Convert the stories to sentiment trajectories [n_samples, n_groups]
         #Values contain 0, 1, 2 for neg, neutral and positive sentiment
         trajectories = self.find_trajectories(stories)
-        #trajectories = np.sign(trajectories).astype(int) + 1 #+1 to shift from [-1,1] to [0,2]
                 
+        #___ Counting classifier       
         #Returns a [n_values, ..., n_values] where len(...) = len(story_grouping)
-        self.counts = self.count_elements(trajectories, self.n_values, smoothing = 5)
-        
-        print(self.counts)
+        #self.counts = self.count_elements(trajectories, self.n_values, smoothing = 5)
+        #___ SVC classifier
+        self.classifier.fit(trajectories[:,:-1], trajectories[:,-1])
+        #___
         
         pass
 
     def predict(self, data: np.ndarray) -> str:
+        #Decompose data
+        #--> data[:,0] contains ID'sa
+        #--> data[:,1-5] contains first 4 sentences
+        #--> data[:,5-7] contains 2 ending options
         IDs = data[:,0]
-        partial_stories = data[:,1:5]
+        partial_stories = data[:,5-(self.n_sentences-1):5]
         endings = data[:,5:7]
         
         # IMPORTANT: group test set exectly the same as when training otherwise 
         # nothing makes sense! Except for the last sentence which by definition
-        partial_stories = self.group_stories(partial_stories, self.story_grouping[:-1])
+        # of test set cannot be in the grouping
+        partial_stories = self.group_stories(partial_stories,
+                                    self.story_grouping[:-1])
         
         sentiment_for_endings = self.find_trajectories(endings)
-        ending_distributions = self.find_ending_distribution(partial_stories)
+        sentiment_distributions = self.find_ending_distribution(partial_stories)
         
-        #TODO: fix the code a bit
-        endings_probabilities = []
-        for i, endings in enumerate(sentiment_for_endings):
-            distribution = ending_distributions[i]
-            mapped_ending = [distribution[ending] for ending in endings]
-            endings_probabilities.append(mapped_ending)        
-        endings_probabilities = np.array(endings_probabilities)
-        
+        endings_probabilities = self.map_probabilities(sentiment_for_endings, sentiment_distributions)
+                
         # +1 to select sentence
-        return np.argmax(endings_probabilities, axis=1) + 1 
+        return np.argmax(endings_probabilities, axis=1) + 1
 
+    def map_probabilities(self, indices, distribution):
+        #The one-liner to rule them all
+        return [distr[indices] for (indices, distr) in list(zip(indices, distribution))]
+    
     def find_ending_distribution(self, partial_stories):
         partial_trajectories = self.find_trajectories(partial_stories)
-        return [self.counts[tuple(partial_trajectory)] for partial_trajectory in partial_trajectories]
+        #return [self.counts[tuple(partial_trajectory)] for partial_trajectory in partial_trajectories]
+        return self.classifier.predict_proba(partial_trajectories)
         
     def count_elements(self, objs, n_values, smoothing=1):
         #Method assumes that values are in range [0,n_values] (n_values excluded)
         n_objs, n_steps = objs.shape
         shape = (n_values,)*n_steps
         
+        #Smoothing changes nothing at this moment (count not used as prior)
         counts = np.full(shape, smoothing)
         
         for obj in objs:
@@ -91,7 +102,6 @@ class SentimentTracjectoryStrategy(Strategy):
         self.done +=1
         if self.done % 1000 == 0:
             print("Done {0}".format(self.done))
-        self.done=0
         #Sanity
         
         return trajectory
@@ -106,6 +116,7 @@ class SentimentTracjectoryStrategy(Strategy):
         if n_feat != np.sum(grouping):
             raise ValueError('Cannot group, expected {0} sentences per sample but received {1}'.format(np.sum(grouping), n_feat))
 
+        #TODO: make nicer
         grouped_data = []
         low_ind = 0
         for group_size in grouping:
