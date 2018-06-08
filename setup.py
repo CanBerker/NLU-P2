0fc6a0aa17ef64fb5dd2d527784be87c0299cc03
Renato <renatoj.marroquin@gmail.com>
2018-06-08 18:33:43 +0200

    Adding

--- setup.py
+++ setup.py
@@ -3,14 +3,13 @@ import os
 
 def pip_depts():
     pip_cmd = "pip install -r requirements"
-    os.execute(pip_cmd)
+    os.system(pip_cmd)
 
 def nltk_deps():
     nltk.download('punkt')
     nltk.download('averaged_perceptron_tagger')
     nltk.download('vader_lexicon')
-    # TODO add pos tager
 
 if __name__ == "__main__":
-    #pip_deps()
+    pip_deps()
     nltk_deps()
