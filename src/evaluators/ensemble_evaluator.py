8b77267016db68b121bb74df9e40ddc339d6c6ea
Renato <renatoj.marroquin@gmail.com>
2018-06-08 20:05:04 +0200

    Changes

--- src/evaluators/ensemble_evaluator.py
+++ src/evaluators/ensemble_evaluator.py
@@ -4,16 +4,26 @@ from strategies import Strategy
 
 class EnsembleEvaluator(object):
     def __init__(self, training_data: np.ndarray,
-                 validation_data: np.ndarray, augmented_data: np.ndarray):
+                 validation_data: np.ndarray, augmented_data: np.ndarray, validate_on_eth = False):
         self.training_data = training_data
         self.validation_data = validation_data
         self.augmented_data = augmented_data
+        self.validate_on_eth = validate_on_eth
 
     def validation_error(self, strategy: Strategy) -> float:
         val_stories, val_labels = np.split(self.validation_data, [-1], axis=1 )
+        print("val_stories", val_stories.shape)
+        print("val_labels", val_labels.shape)
         val_labels = np.squeeze(val_labels)
 
         strategy.fit(self.training_data, self.validation_data, self.augmented_data)
         predictions = strategy.predict(val_stories)
 
-        return np.mean(np.equal(predictions, val_labels.astype(int)))
+        if self.validate_on_eth:
+            with open("nlu_test.out", "w+") as f:
+                [f.write("{0}\n".format(p)) for p in predictions]
+
+        pred = 0.0
+        if not self.validate_on_eth:
+            pred = np.mean(np.equal(predictions, val_labels.astype(int)))
+        return pred
